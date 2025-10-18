ada26eb89c2c9a8a7cddbcabfab8ee2d839e43be
MaximusHill <ST20324344@outlook.cardiff.ac.uk>
2025-10-18 20:31:57 +0100

    IfYouHaveTime

--- YearsNamesMarksArray/YearsNamesMarksArray/YearsNamesMarksArray.cpp
+++ YearsNamesMarksArray/YearsNamesMarksArray/YearsNamesMarksArray.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<iostream>
 
 
 int main()
@@ -8,19 +9,34 @@ int main()
 	int arraynum;
 	char name[20];
 	int count=0;
+	int average=0;
+	int Pass=0;
+	int Fail=0;
+	
+	printf("please enter the students name:\n");
+	scanf_s("%s", name, 20);
 	printf("please enter the students year:\n");
 	scanf_s("%d", &year);
 	if (year>0)
 	{
-		printf("please enter the students name:\n");
-		scanf_s("%s", name,20);
 		printf("How many subjects has this student taken:\n");
 		scanf_s("%d", &arraynum);
 		int *Array = new int[arraynum];
+		int *Array2 = new int[arraynum];
 		while(count<arraynum)
 		{
 			printf("Please enter the students mark:\n");
 			scanf_s("%d", &Array[count]);
+			if(Array[count]<50)
+			{
+				Array2[count] = 0;
+				Fail++;
+			}
+			else
+			{
+				Array2[count] = 1;
+				Pass++;
+			}
 			count++;
 		}
 		printf("This is the data stored\n");
@@ -28,7 +44,24 @@ int main()
 		for (int i = 0; i < count; i++)
 		{
 			printf("Mark %d: %d\n",i + 1, Array[i]);
+			if (Array2[i] == 0)
+			{
+				printf("Fail\n");
+				
+			}
+			else 
+			{
+				printf("Pass\n");
+				
+			}
+			
+			average = average + Array[i];
 		}
+		int PassPer = (Pass / arraynum) * 100;
+		int FailPer = (Fail / arraynum) * 100;
+		average = average / count;
+		printf("The average for this year was :%d\n", average);
+		printf("%s has a pass rate of %d and a fail rate of %d",name,PassPer,FailPer);
 	}
 	else
 	{
